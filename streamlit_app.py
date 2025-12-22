a8b7a449af4cf7e5c6bd3dba39de8e83bf489be6
abdurrahman05110-lang <abdur.rahman05110@gmail.com>
2025-12-22 12:49:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
-import streamlit as st
-
-st.title("🎈 UJANG")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+stoichia/
+│
+├── app.py
+├── requirements.txt
+├── README.md
